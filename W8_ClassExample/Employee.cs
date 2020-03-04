7b4b040a1080ecfdd08be46a2e8708ede68d4e0f
bilal amir <xboxforme27@LIVE.CA>
2020-03-04 18:33:30 -0500

    classes

---
+++ W8_ClassExample/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W8_ClassExample
+{
+    class Employee
+    {
+    }
+}
---
+++ W8_ClassExample/Salary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W8_ClassExample
+{
+    class Salary
+    {
+        public string FirstName { get; set; }
+
+    }
+}
