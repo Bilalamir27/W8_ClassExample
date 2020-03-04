4e969291b9576d83c2d5c05a87e1431c342e5e8e
bilal amir <xboxforme27@LIVE.CA>
2020-03-04 18:14:21 -0500

    Add project files.

---
+++ W8_ClassExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W8_ClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
